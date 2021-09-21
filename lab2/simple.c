cd933189b4be4f25bf14b3933c74f1aad8af1b1c
Enkh-Amar Ganbat <enkhamar1220@gmail.com>
2021-09-22 00:26:08 +0800

    lab 2

--- lab2/simple.c
+++ lab2/simple.c
@@ -2,6 +2,18 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#define DRIVER_LIC "GPL"
+#define DRIVER_DESCRIPTION "Simple Module"
+#define DRIVER_AUTHOR "Enkh-Amar.G"
+
+/*
+ * "make" to compile
+ * "sudo insmod simple.ko" to load kernel module
+ * "sudo rmmod simple" to unload kernel module
+ * "make clean" to clean the compiled products
+ * "sudo dmesg" to show the kernel log
+ * "sudo dmesg -c" to clear the kernel log
+ **/
 
 static int __init simple_init(void) {
     printk(KERN_INFO "Simple module has been loaded\n");
@@ -15,6 +27,6 @@ static void __exit simple_exit(void) {
 module_init(simple_init);
 module_exit(simple_exit);
 
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Simple Module");
-MODULE_AUTHOR("SGG");
+MODULE_LICENSE(DRIVER_LIC);
+MODULE_DESCRIPTION(DRIVER_DESCRIPTION);
+MODULE_AUTHOR(DRIVER_AUTHOR);
