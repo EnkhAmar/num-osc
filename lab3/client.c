c8058e6736acdb07940660da7e6cc98123fb7ebd
EnkhAmar <enkhamar1220@gmail.com>
2021-10-04 01:33:35 +0800

    lab 3

--- lab3/client.c
+++ lab3/client.c
@@ -5,39 +5,65 @@
 #include <string.h>
 #include <stdlib.h>
 #define PORT 8080
+#define MAX_LIMIT 1024
 
+char *getInput();
+   
 int main(int argc, char const *argv[])
 {
-	int sock = 0, valread;
-	struct sockaddr_in serv_addr;
-	char input[1024];
-	fgets(input, 1024, stdin);
-	char *msg = (char *)malloc(sizeof(char) * (strlen(input)));
-	char buffer[1024] = {0};
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf("\n Socket creation error \n");
-		return -1;
-	}
-
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
+	int msg_count = 3;
+		int sock = 0, valread;
+		struct sockaddr_in serv_addr;
+		// char *hello = getInput();
+		// char buffer[MAX_LIMIT] = {0};
+		if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		{
+			printf("\n Socket creation error \n");
+			return -1;
+		}
 	
-	// Convert IPv4 and IPv6 addresses from text to binary form
-	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-	{
-		printf("\nInvalid address/ Address not supported \n");
-		return -1;
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_port = htons(PORT);
+		
+		// Convert IPv4 and IPv6 addresses from text to binary form
+		if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+		{
+			printf("\nInvalid address/ Address not supported \n");
+			return -1;
+		}
+	
+		if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+		{
+			printf("\nConnection Failed \n");
+			return -1;
+		}
+	while (msg_count > 0) {
+		char *hello = getInput();
+		char buffer[MAX_LIMIT] = {0};
+		send(sock , hello , strlen(hello) , 0 );
+		printf("Client: %s\n", hello);
+		valread = read( sock , buffer, MAX_LIMIT);
+		printf("Server: %s\n",buffer );
+		free(hello);
+		msg_count--;
 	}
+    return 0;
+}
 
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-	{
-		printf("\nConnection Failed \n");
-		return -1;
-	}
-	send(sock , msg , strlen(msg) , 0 );
-	printf("Hello message sent\n");
-	valread = read( sock , buffer, 1024);
-	printf("%s\n",buffer );
-	return 0;
+char *getInput() {
+  int n = 0;
+  char input[MAX_LIMIT];
+  char *str;
+
+  printf(">> ");
+  fgets(input, MAX_LIMIT, stdin);
+  int len = strlen(input) - 1;     // I had to minus 1, bcz "\n" is counted as character
+
+  str = (char *)malloc(sizeof(char)*(len));  
+
+  for (int i=0; i<len; i++) {
+    str[i] = input[i]; 
+  }
+
+  return (char *)str;
 }
--- lab3/server.c
+++ lab3/server.c
@@ -5,56 +5,83 @@
 #include <netinet/in.h>
 #include <string.h>
 #define PORT 8080
+#define MAX_LIMIT 1024
+
+char *getInput();
+
 int main(int argc, char const *argv[])
 {
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
-	char buffer[1024] = {0};
-	char input[1024];
-	fgets(input, 1024, stdin);
-	char *msg = (char *)malloc(sizeof(char) * (strlen(input)));
-	
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
+	int msg_count = 3;
+		int server_fd, new_socket, valread;
+		struct sockaddr_in address;
+		int opt = 1;
+		int addrlen = sizeof(address);
+		// char buffer[MAX_LIMIT] = {0};
+		// char *hello = getInput();
+		
+		// Creating socket file descriptor
+		if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+		{
+			perror("socket failed");
+			exit(EXIT_FAILURE);
+		}
+		
+		// Forcefully attaching socket to the port 8080
+		if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+													&opt, sizeof(opt)))
+		{
+			perror("setsockopt");
+			exit(EXIT_FAILURE);
+		}
+		address.sin_family = AF_INET;
+		address.sin_addr.s_addr = INADDR_ANY;
+		address.sin_port = htons( PORT );
+		
+		// Forcefully attaching socket to the port 8080
+		if (bind(server_fd, (struct sockaddr *)&address, 
+									sizeof(address))<0)
+		{
+			perror("bind failed");
+			exit(EXIT_FAILURE);
+		}
+		if (listen(server_fd, 3) < 0)
+		{
+			perror("listen");
+			exit(EXIT_FAILURE);
+		}
+		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+						(socklen_t*)&addrlen))<0)
+		{
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+	while(msg_count > 0) {
+		char *hello = getInput();
+		char buffer[MAX_LIMIT] = {0};
+		valread = read( new_socket , buffer, MAX_LIMIT);
+		printf("Client: %s\n",buffer );
+		send(new_socket , hello , strlen(hello) , 0 );
+		printf("Server: %s\n", hello);
+		free(hello);
+		msg_count--;
 	}
-	
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-												&opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons( PORT );
-	
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr *)&address,
-								sizeof(address))<0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-					(socklen_t*)&addrlen))<0)
-	{
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	valread = read( new_socket , buffer, 1024);
-	printf("%s\n",buffer );
-	send(new_socket , msg , strlen(msg) , 0 );
-	printf("Hello message sent\n");
-	return 0;
+    return 0;
+}
+
+char *getInput() {
+  int n = 0;
+  char input[MAX_LIMIT];
+  char *str;
+
+  printf(">> ");
+  fgets(input, MAX_LIMIT, stdin);
+  int len = strlen(input) - 1;     // I had to minus 1, bcz "\n" is counted as character
+
+  str = (char *)malloc(sizeof(char)*(len));  
+
+  for (int i=0; i<len; i++) {
+    str[i] = input[i]; 
+  }
+
+  return (char *)str;
 }
